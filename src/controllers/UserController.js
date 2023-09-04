80990b8d05f874fa4e152aed156189f78132a12d
Artem Ibragimov <aribragimov@inbox.ru>
2023-09-04 13:04:37 +0300

    created authorizations and User Controller

---
+++ src/controllers/UserController.js
@@ -0,0 +1,102 @@
+import {validationResult} from "express-validator";
+import bcrypt from "bcrypt";
+import UserModel from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            res.status(400).json(errors.array())
+        }
+
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModel({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            passwordHash: hash,
+            avatar: req.body.avatar
+        })
+
+        const user = await doc.save()
+
+        const token = jwt.sign({
+                _id: user._id
+            },
+            'secret123',
+            {
+                expiresIn: '30d'
+            })
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: 'Не удалось зарегестрироваться'
+        })
+    }
+}
+export const login = async (req, res) => {
+    try {
+        const user = await UserModel.findOne({email: req.body.email})
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль'
+            })
+        }
+
+        const token = jwt.sign({
+                _id: user._id
+            },
+            'secret123',
+            {
+                expiresIn: '30d'
+            })
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: 'Не удалось авторизоваться'
+        })
+    }
+}
+export const me = async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId)
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден'
+            })
+        }
+        const {passwordHash, ...userData} = user._doc
+        res.json({
+            ...userData
+        })
+    } catch (err) {
+        return res.status(404).json({
+            message: 'Пользователь не найден'
+        })
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
-import {registerValidations} from "./validations/auth.js";
-import {validationResult} from 'express-validator'
-import UserModel from './models/User.js'
+import {registerValidations} from "./validations/auth.js"
+import checkAuth from "./utils/checkAuth.js"
+import * as UserController from "./controllers/UserController.js";
 
 mongoose.connect('mongodb+srv://aribragimov:17052002Artem@cluster0.9ivw93j.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
@@ -18,46 +16,9 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
-app.post('/auth/reg', registerValidations, async (req, res) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            res.status(400).json(errors.array())
-        }
-
-        const password = req.body.password
-        const salt = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(password, salt)
-
-        const doc = new UserModel({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            passwordHash: hash,
-            avatar: req.body.avatar
-        })
-
-        const user = await doc.save()
-
-        const token = jwt.sign({
-                _id: user._id
-            },
-            'secret123',
-            {
-                expiresIn: '30d'
-            })
-
-        const {passwordHash, ...userData} = user._doc
-
-        res.json({
-            ...userData,
-            token
-        })
-    } catch (err) {
-        res.status(500).json({
-            message: 'Не удалось зарегестрироваться'
-        })
-    }
-})
+app.post('/auth/login', UserController.login)
+app.post('/auth/register', registerValidations, UserController.register)
+app.get('/auth/me', checkAuth, UserController.me)
 
 app.listen(port, () => {
     console.log(`Server OK`)
