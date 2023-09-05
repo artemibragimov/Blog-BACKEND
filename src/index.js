43124c2dde6fc0ac069581d243aaecf289929b57
Artem Ibragimov <aribragimov@inbox.ru>
2023-09-05 15:29:05 +0300

    install multer, upload images function added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import multer from 'multer'
 import mongoose from 'mongoose'
 import {loginValidations, postCreateValidations, registerValidations} from "./validations/validations.js"
 import checkAuth from "./utils/checkAuth.js"
 import * as UserController from "./controllers/UserController.js";
 import * as PostController from "./controllers/PostController.js";
+
+const app = express()
+const port = 3000
+app.use(express.json())
+app.use('/uploads', express.static('C:\\CODE\\Blog-BACKEND\\src\\uploads'))
 
 mongoose.connect('mongodb+srv://aribragimov:17052002Artem@cluster0.9ivw93j.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
@@ -13,16 +19,29 @@ mongoose.connect('mongodb+srv://aribragimov:17052002Artem@cluster0.9ivw93j.mongo
         console.log('DB error', err)
     })
 
-const app = express()
-const port = 3000
-app.use(express.json())
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'C:\\CODE\\Blog-BACKEND\\src\\uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({storage: storage})
+
 
 app.post('/auth/login', loginValidations, UserController.login)
 app.post('/auth/register', registerValidations, UserController.register)
 app.get('/auth/me', checkAuth, UserController.me)
 
-app.get('/posts', checkAuth, PostController.getAll)
-app.get('/posts/:id', checkAuth, PostController.getOne)
+app.post('/uploads', upload.single('image'), (req, res) => {
+    res.json({
+        success: true
+    })
+})
+
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidations, PostController.createPost)
 app.delete('/posts/:id', checkAuth, PostController.remove)
 app.put('/posts/:id', checkAuth, PostController.update)
